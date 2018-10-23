dc2b7f97021ded7499ce21bb8d8a37102682ea62
Richard Case <richard.case@outlook.com>
2018-10-23 12:50:44 +0100

    Fix linting
    Signed-off-by: Richard Case <richard.case@outlook.com>

--- pkg/env/env.go
+++ pkg/env/env.go
@@ -23,9 +23,8 @@ func GetEnvVarValue(client kubernetes.Interface, namespace string, from *sls.Env
 	return "", fmt.Errorf("invalid valueFrom")
 }
 
-// See https://github.com/kubernetes/kubernetes/blob/137193faafbc3eadf4ef5ce62bbfcd0f2e7ceca9/pkg/kubectl/cmd/set/env/env_resolve.go#L50
-// not implementing the store so that we query every time
-func getSecretValue(client kubernetes.Interface, namespace string, secretSelector *corev1.SecretKeySelector) (string, error) {
+func getSecretValue(client kubernetes.Interface, namespace string,
+	secretSelector *corev1.SecretKeySelector) (string, error) {
 	secret, err := client.CoreV1().Secrets(namespace).Get(secretSelector.Name, metav1.GetOptions{})
 	if err != nil {
 		return "", errors.Wrap(err, "getting secret")
@@ -36,15 +35,14 @@ func getSecretValue(client kubernetes.Interface, namespace string, secretSelecto
 	return "", fmt.Errorf("key %s not found in secret %s", secretSelector.Key, secretSelector.Name)
 }
 
-// See https://github.com/kubernetes/kubernetes/blob/137193faafbc3eadf4ef5ce62bbfcd0f2e7ceca9/pkg/kubectl/cmd/set/env/env_resolve.go#L68
-// not implementing the store so that we query time
-func getConfigValue(client kubernetes.Interface, namespace string, configSelector *corev1.ConfigMapKeySelector) (string, error) {
+func getConfigValue(client kubernetes.Interface, namespace string,
+	configSelector *corev1.ConfigMapKeySelector) (string, error) {
 	configMap, err := client.CoreV1().ConfigMaps(namespace).Get(configSelector.Name, metav1.GetOptions{})
 	if err != nil {
 		return "", errors.Wrap(err, "getting config map")
 	}
 	if data, ok := configMap.Data[configSelector.Key]; ok {
-		return string(data), nil
+		return data, nil
 	}
 	return "", fmt.Errorf("key %s not found in configmap %s", configSelector.Key, configSelector.Name)
 }
